278851bdeb02d987150ef17d2af73dc6d8a9bea2
Shaman Ando <you@example.com>
2022-09-03 20:54:00 +0400

    add Exercise10

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число четным. 
+
+Console.WriteLine("vvedite chislo");
+string numberString = Console.ReadLine()!;
+int number = int.Parse(numberString);
+
+if (number%2 == 0)
+{
+    Console.WriteLine("chetnoe");
+}
+else
+{
+    Console.WriteLine("ne chetnoe");
+}
