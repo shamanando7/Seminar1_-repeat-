2ecf70189a1ca65ce2800b6f8b382cc1489511e4
Shaman Ando <you@example.com>
2022-09-03 19:11:42 +0400

    add Exercise5

---
+++ Exercise_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+//456 -> 6
+
+Console.WriteLine("Vvedite chislo");
+string numberString = Console.ReadLine()!;
+int number = int.Parse(numberString);
+
+int i = number%10;
+
+Console.WriteLine(i);
+
