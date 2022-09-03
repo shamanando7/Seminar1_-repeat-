77dd516b55f6a8247c65e56bf619522a00d7db7d
Shaman Ando <you@example.com>
2022-09-03 23:06:30 +0400

    add Exercise13

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("vvedite chislo");
+string numberString = Console.ReadLine()!;
+int number = int.Parse(numberString);
+
+if (number%7 == 0 && number%23 == 0)
+{
+ Console.WriteLine("kratno");
+}
+
+else
+{
+    Console.WriteLine("ne kratno");
+}
