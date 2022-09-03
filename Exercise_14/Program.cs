08186441117f63c917aaca069c75fc399870515c
Shaman Ando <you@example.com>
2022-09-03 23:19:22 +0400

    add Exercise14

---
+++ Exercise_14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом второго.
+
+
+Console.WriteLine("vvedite pervoe chislo");
+string numberString = Console.ReadLine()!;
+int number = int.Parse(numberString);
+
+Console.WriteLine("vvedite vtoroe chislo");
+string numberString2 = Console.ReadLine()!;
+int number2 = int.Parse(numberString2);
+
+if (number*number == number2)
+{
+   Console.WriteLine("chislo yavlaetsa kvadratom"); 
+}
+
+else
+{
+   Console.WriteLine("chislo ne yavlaetsa kvadratom"); 
+}
