53bba7fec408f104e9fe4ebd488577afbcf3594f
Shaman Ando <you@example.com>
2022-09-03 20:28:53 +0400

    add Exercise9

---
+++ Exercise_9/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти максимальное из трех чисел.
+
+
+Console.WriteLine("vvedite pervoe chislo");
+string numberString1 = Console.ReadLine()!;
+int number1 = int.Parse(numberString1);
+
+Console.WriteLine("vvedite vtoroe chislo");
+string numberString2 = Console.ReadLine()!;
+int number2 = int.Parse(numberString2);
+
+Console.WriteLine("vvedite tret'e chislo");
+string numberString3 = Console.ReadLine()!;
+int number3 = int.Parse(numberString3);
+
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine("max " + number1);
+}
+
+else if (number2 > number3)
+{
+         Console.WriteLine("max " + number2);
+}
+
+else
+{
+    Console.WriteLine("max " + number3);
+}
