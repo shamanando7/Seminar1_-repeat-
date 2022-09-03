4b674a733789c267f632a31c23bc4c16955f5b7a
Shaman Ando <you@example.com>
2022-09-03 13:55:04 +0400

    add Exercise

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,63 @@
+﻿//  Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго. 
+
+// a = 25, b =  5 -> да
+
+
+// Console.WriteLine("Введите первое число");
+// string numberString1 = Console.ReadLine()!;
+// int number1 = int.Parse(numberString1);
+
+// Console.WriteLine("Введите второе число");
+// string numberString2 = Console.ReadLine()!;
+// int number2 = int.Parse(numberString2);
+
+// if (number2 == number1 / number2)
+//    {
+//     Console.WriteLine("да");
+//    }
+// else
+// {
+//     Console.WriteLine("нет");
+// }
+
+
+
+
+// Console.WriteLine("Введите первое число");
+// string numberString1 = Console.ReadLine()!;
+// int number1 = int.Parse(numberString1);
+
+// Console.WriteLine("Введите второе число");
+// string numberString2 = Console.ReadLine()!;
+// int number2 = int.Parse(numberString2);
+
+// if (number2 == number1/number2)
+// {
+//     Console.WriteLine("да");
+// }
+// else
+// {
+// Console.WriteLine("нет");
+// }
+
+
+
+
+
+Console.WriteLine("Введите первое число");
+string numberstring1 = Console.ReadLine()!;
+int number1 = int.Parse(numberstring1);
+
+Console.WriteLine("Введите второе число");
+string numberstring2 = Console.ReadLine()!;
+int number2 = int.Parse(numberstring2);
+
+if (number2 == number1/number2)
+{
+Console.WriteLine("yes");
+}
+
+else 
+{
+    Console.WriteLine("no");
+}
