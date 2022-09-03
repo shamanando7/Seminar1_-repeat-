d85fc35843bc244a10a6b5ce5dbdf86fc1bb18c6
Shaman Ando <you@example.com>
2022-09-03 20:47:23 +0400

    add Exercise

--- Exercise_9/Program.cs
+++ Exercise_9/Program.cs
@@ -1,6 +1,35 @@
 ﻿// Найти максимальное из трех чисел.
 
 
+// Console.WriteLine("vvedite pervoe chislo");
+// string numberString1 = Console.ReadLine()!;
+// int number1 = int.Parse(numberString1);
+
+// Console.WriteLine("vvedite vtoroe chislo");
+// string numberString2 = Console.ReadLine()!;
+// int number2 = int.Parse(numberString2);
+
+// Console.WriteLine("vvedite tret'e chislo");
+// string numberString3 = Console.ReadLine()!;
+// int number3 = int.Parse(numberString3);
+
+
+// if (number1 > number2 && number1 > number3)
+// {
+//     Console.WriteLine("max " + number1);
+// }
+
+// else if (number2 > number3)
+// {
+//          Console.WriteLine("max " + number2);
+// }
+
+// else
+// {
+//     Console.WriteLine("max " + number3);
+// }
+
+
 Console.WriteLine("vvedite pervoe chislo");
 string numberString1 = Console.ReadLine()!;
 int number1 = int.Parse(numberString1);
@@ -13,18 +42,32 @@ Console.WriteLine("vvedite tret'e chislo");
 string numberString3 = Console.ReadLine()!;
 int number3 = int.Parse(numberString3);
 
+int max = number1;
 
-if (number1 > number2 && number1 > number3)
-{
-    Console.WriteLine("max " + number1);
-}
+if (max < number2)
+    max = number2;
+else 
+max = number1;
 
-else if (number2 > number3)
-{
-         Console.WriteLine("max " + number2);
-}
+if (max < number3)
+    max = number3;
+  
+Console.WriteLine(max);
 
-else
-{
-    Console.WriteLine("max " + number3);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
