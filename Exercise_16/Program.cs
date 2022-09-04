dd4af6e5bb96d5a44c45652eb3aeefd04cfe4f36
Shaman Ando <you@example.com>
2022-09-04 23:40:24 +0400

    add Exercise16

---
+++ Exercise_16/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+
+
+Console.WriteLine("Пожалуйста, введите целое число");
+int numberA = int.Parse(Console.ReadLine()!);
+
+while (numberA > 1000)
+{
+    numberA = numberA / 10;
+}
+
+
+int numberB = numberA / 100;
+
+if (numberB == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int numberC = numberA - ((numberA / 10) * 10);
+    Console.WriteLine("Третья цифра введенного числа " + numberC);
+}
