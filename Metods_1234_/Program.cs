d0056cdf970ee427886906a8339dd6b1977c8a80
Shaman Ando <you@example.com>
2022-09-04 13:34:30 +0400

    add Metods

---
+++ Metods_1234_/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+// void Metod1()
+// {
+//     Console.WriteLine("Autor...");
+// }
+
+// Metod1();
+
+
+
+// void Metod2 (string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// Metod2 ("what?");
+
+
+// void Metod21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i<count)
+//     {
+//     Console.WriteLine(msg); // где переменная count отображает на экране определенное количество сообщений msg.
+//     i++;
+//     }
+// }
+
+// Metod21(count: 4, msg:"Text");
+
+
+
+// int Metod3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Metod3();
+// Console.WriteLine(year);
+
+
+// string Metod4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while(i<count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Metod4(10, "asdf");
+// Console.WriteLine(res);
+
