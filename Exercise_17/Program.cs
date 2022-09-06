9cd9345166c5fcfc03686837dff5eb189f8d7bf0
Shaman Ando <you@example.com>
2022-09-06 09:25:55 +0400

    add Exercise17

---
+++ Exercise_17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("vvedite chislo");
+string numberString = Console.ReadLine()!;
+int number = int.Parse(numberString);
+
+if (number < 5)
+{
+    Console.WriteLine("ne yavlyaetsa vyhodnim");
+}
+
+
+else if (number == 6)
+{
+    Console.WriteLine("yavlyaetsa vyhodnym");
+}
+else if (number == 7)
+{
+    Console.WriteLine("yavlyaetsa vyhodnym");
+}
+
+else if (number < 1)
+{
+    Console.WriteLine("vvedeno nekorrectnoe znachenie");
+}
