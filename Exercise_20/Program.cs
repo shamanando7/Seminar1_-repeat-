d56a2302b9fcacb1fa0a355f0671bf5aa4e6de53
Shaman Ando <you@example.com>
2022-09-06 15:49:24 +0400

    add Exercise20

--- Exercise_20/Program.cs
+++ Exercise_20/Program.cs
@@ -4,29 +4,34 @@
 // A (7,-5); B (1,-1) -> 7,21
 
 
-Console.WriteLine("Введите координатy A: ");
-var AString = Console.ReadLine();
-double A = double.Parse(AString);
+Console.WriteLine("Введите координатy x1: ");
+double x1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите координату B: ");
-var BString = Console.ReadLine();
-double B = double.Parse(BString);
+Console.WriteLine("Введите координату y1: ");
+double y1 = double.Parse(Console.ReadLine());
 
-double sqrt = Math.Sqrt(A + B);
+Console.WriteLine("Введите координатy x2: ");
+double x2 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Расстояние между двумя точками " + sqrt.ToString("N2")); // округление
-
-
+Console.WriteLine("Введите координату y2: ");
+double y2 = double.Parse(Console.ReadLine());
 
 
- // мое творение. 
+
+double AB = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+Console.WriteLine("Расстояние между двумя точками " + AB.ToString("N2")); // округление
 
 
+
+// мое творение. 
 // найти квадратный корень:
 
 // int a = 25;
 // int sqrt = (int) Math.Sqrt(a);
 // Console.WriteLine(sqrt);
+
+
 
 
 // или
