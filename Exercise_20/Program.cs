a321b39e964205790a39727adc865d51c6638d57
Shaman Ando <you@example.com>
2022-09-06 15:26:41 +0400

    add Exercise20

---
+++ Exercise_20/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+
+Console.WriteLine("Введите координатy A: ");
+var AString = Console.ReadLine();
+double A = double.Parse(AString);
+
+Console.WriteLine("Введите координату B: ");
+var BString = Console.ReadLine();
+double B = double.Parse(BString);
+
+double sqrt = Math.Sqrt(A + B);
+
+Console.WriteLine("Расстояние между двумя точками " + sqrt.ToString("N2")); // округление
+
+
+
+
+ // мое творение. 
+
+
+// найти квадратный корень:
+
+// int a = 25;
+// int sqrt = (int) Math.Sqrt(a);
+// Console.WriteLine(sqrt);
+
+
+// или
+
+// int a = 25;
+// double sqrt = (int) Math.Sqrt(a);
+// Console.WriteLine(sqrt);
