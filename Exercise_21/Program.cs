e17a350151dcfa99d98654efc09847a660bd9dc1
Shaman Ando <you@example.com>
2022-09-08 14:06:19 +0400

    add exercise21

---
+++ Exercise_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+
+int SummaChisel(int num)
+{
+    int summ = 0;
+    for (int i = 1; i < num; i++)
+    {
+        summ = i + summ;
+    }
+    return summ;
+}
+
+Console.WriteLine("vvedite chislo");
+string numberString = Console.ReadLine();
+int number = int.Parse(numberString);
+
+if (number <=0)
+Console.WriteLine("not correct");
+return;
+
+int result = SummaChisel(number);
+Console.WriteLine(result);
