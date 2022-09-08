6a8248f551a4b742fcecc8ee6abcfdb7e2267947
Shaman Ando <you@example.com>
2022-09-08 18:23:13 +0400

    add Exercise24

---
+++ Exercise_24/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+int[] CreateArray()
+{
+    int[] array = new int[8];
+    var random = new Random();
+
+    for (int i = 0; i < 8; i++)
+        array[i] = random.Next(0, 2);
+
+    return array;
+}
+
+
+var array =
+CreateArray(); 
+for (int i = 0; i < 8; i++)
+    Console.Write(array[i]);
+
+// создали функцию которая создает массив и выводит на экран.
