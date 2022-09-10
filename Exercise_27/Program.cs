d6f0272e4c5152d49f6e3b7202d8ea87c7c595eb
Shaman Ando <you@example.com>
2022-09-10 17:17:32 +0400

    add Exercise27

---
+++ Exercise_27/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные и наоборот. 
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+
+int[] createArray()
+{
+    var random = new Random();
+    var result = new int[15];
+    for (long i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(-100, 100);
+    }
+    return result;
+
+}
+
+void PositiveZamena(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+}
+
+
+
+var array = createArray();                 // Создается массив
+for (int i = 0; i < array.Length; i++)     // Массив выводится
+{
+    Console.Write(array[i] + ", ");
+}
+
+PositiveZamena(array);                     // Вызывается функция (никакое значение не присваивается потому что 
+Console.WriteLine();                       // функция void(просто меняются данные в массиве, того массива который в нее передан)) 
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+
+
+
+
